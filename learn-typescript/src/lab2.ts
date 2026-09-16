cc7bdee3bd6a5d72e5bb476efdef0eeb5f01ea41
trongden <trongnafnaf@gmail.com>
2026-09-16 19:50:42 +0700

    lab2 bai2

--- learn-typescript/src/lab2.ts
+++ learn-typescript/src/lab2.ts
@@ -1,12 +1,13 @@
 enum Rate {
     low = 'Thấp', medium = 'Trung bình', high = 'Cao'
 }
-let product: {
+type Product = {
     name: string;
     price: number;
     sale: boolean;
     rate: Rate;
-} = {
+} 
+let product: Product = {
     name: "Trong",
     price: 100,
     sale: true,
@@ -14,3 +15,48 @@ let product: {
 }
 
 console.log(product);
+
+let listProduct: Array<Product> = [
+    {
+        name: "Trong",
+        price: 9,
+        sale: true,
+        rate: Rate.low
+    },
+    {
+        name: "laptop",
+        price: 6,
+        sale: true,
+        rate: Rate.high
+    },
+    {
+        name: "balo",
+        price: 3,
+        sale: false,
+        rate: Rate.medium
+    },
+    {
+        name: "áo",
+        price: 2,
+        sale: false,
+        rate: Rate.medium
+    },
+    {
+        name: "quần",
+        price: 1,
+        sale: false,
+        rate: Rate.medium
+    }
+]
+console.log(listProduct);
+
+function description(list: typeof listProduct){
+    return list.map((item) => ({
+        ...item,
+        description: item.price>5? 'Tốt' : 'Bình thường'
+    }))
+}
+
+console.log(description(listProduct));
+
+
