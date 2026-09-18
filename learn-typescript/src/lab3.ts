a30f03b978c8f259993b342b6d563d4683290522
trongden <trongnafnaf@gmail.com>
2026-09-18 20:06:07 +0700

    lab3

---
+++ learn-typescript/src/lab3.ts
@@ -0,0 +1,38 @@
+
+//Bài 1
+const averageScore = (...scores: number[]): number =>
+  scores.length === 0 ? 0 : scores.reduce((sum, val) => sum + val, 0) / scores.length;
+console.log(averageScore(2,4,6,8,10));
+
+//Bài 2
+function checkNumber(n: number): string{
+    if (n<=0) return "không hợp lệ";
+    if (n%2 === 0 ) return "số chẵn";
+    else return "số lẻ"
+}
+console.log(checkNumber(5));
+console.log(checkNumber(6));
+
+//Bài 3
+function createUser(name:string,age?:number,role:string = "user"){
+    if (age === undefined) return {name,role}
+    else return {name,age,role}
+}
+console.log(createUser("Trong",20,"admin"));
+console.log(createUser("hoa"));
+
+//Bài 4
+function mergeProducts(arr1: string[],arr2: string[]): string[]{
+    return [...arr1,...arr2];
+}
+
+const a = ["Trong",'Vinh', "Bao"];
+const b = ["Hoa","Tuyen","Van"];
+console.log(mergeProducts(a,b));
+
+function printProducts(...product: string[]){
+    product.forEach((product)=>{
+        console.log(`${product}`);       
+    })
+}
+printProducts(...mergeProducts(a,b));
