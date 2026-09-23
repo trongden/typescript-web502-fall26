a3064b76bf19560afb7b8933f030b2d2522baaf0
trongden <trongnafnaf@gmail.com>
2026-09-23 18:58:28 +0700

    lab4

---
+++ learn-typescript/src/lab4.ts
@@ -0,0 +1,60 @@
+class Student {
+    id: number;
+    name: string;
+    constructor(id: number,name: string){
+        this.id = id;
+        this.name = name;
+    }
+    display(){
+        return `ID: ${this.id}, name: ${this.name}`
+    }
+}
+const p = new Student(1,"Trong");
+
+console.log(p.display());
+
+
+interface User {
+    id: number;
+    email: string;
+    phone?: number;
+}
+
+
+type product = {
+    id: number;
+    name: string;
+    price: number;
+}
+const productItem: product ={
+    id:1,
+    name:"Trong",
+    price:222
+}
+console.log(productItem);
+
+
+type Status = "loading" | "success" | "error";
+function logStatus(status: Status): void {
+    switch (status) {
+        case "loading":
+            break;
+        case "success":
+            break;
+        case "error":
+            break;
+        default:
+            break;
+    }
+}
+logStatus("loading");
+logStatus("success");
+logStatus("error");
+
+
+function identity<T>(value: T): T{
+    return value;
+}
+console.log(identity("Trong"));
+console.log(identity(20));
+console.log(identity(true));
